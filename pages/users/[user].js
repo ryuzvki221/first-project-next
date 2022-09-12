a50d5ee4554cab793166c883c086f9142abf1654
ryuzvki-sn <contact.stateless@gmail.com>
2022-09-12 13:41:12 +0000

    create user dynamic page

--- pages/users/[user].js
+++ pages/users/[user].js
@@ -1,7 +1,90 @@
-import React from 'react'
+import {
+  Container,
+  Image,
+  Center,
+  Heading,
+  Text,
+  VStack,
+  HStack,
+  Tag,
+  Box,
+} from "@chakra-ui/react";
 
-export default function user() {
+import Navbar from "../../components/Navbar/Navbar";
+export default function user({ user }) {
   return (
-    <div>un user</div>
-  )
+    <Container maxW="container.xl" px={4} pt={5}>
+      <Navbar />
+      <Image
+        src="https://via.placeholder.com/150"
+        alt="user"
+        borderRadius="full"
+        boxSize="150px"
+        mx="auto"
+        mt={5}
+      />
+      <Center>
+        <VStack>
+          <Heading>{user.name}</Heading>
+          <Text color="grey">
+            {"@"}
+            {user.username} {"|"} {user.website}
+          </Text>
+          <HStack>
+            <Tag>{user.company.name}</Tag>
+            <Tag>{user.company.catchPhrase}</Tag>
+            <Tag>{user.company.bs}</Tag>
+          </HStack>
+          <Heading>Contact</Heading>
+          <Box shadow="md" borderWidth="1px" p={5} align="left">
+            <Text>
+              Tel{" : "}
+              {user.phone}
+            </Text>
+            <Text>
+              Email{" : "}
+              {user.email}
+            </Text>
+            <Text>
+              Adresse{" : "}
+              {user.address.street}
+              {" | "}
+              {user.address.suite}
+              {" | "}
+              {user.address.city}
+              {" | "}
+              {user.address.zipcode}
+            </Text>
+          </Box>
+        </VStack>
+      </Center>
+    </Container>
+  );
+}
+
+export async function getStaticProps({ params }) {
+  const id = params.user;
+
+  const data = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const user = await data.json();
+
+  return {
+    props: {
+      user,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const data = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await data.json();
+
+  const paths = users.map((item) => ({
+    params: { user: item.id.toString() },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
 }
--- pages/users/index.js
+++ pages/users/index.js
@@ -1,7 +1,65 @@
-import React from 'react'
-
-export default function index() {
+import Navbar from "../../components/Navbar/Navbar";
+import { v4 as uuidv4 } from "uuid";
+import {
+  Box,
+  Container,
+  Heading,
+  Link,
+  SimpleGrid,
+  Text,
+} from "@chakra-ui/react";
+import NextLink from "next/link";
+export default function index({ users }) {
   return (
-    <div>index</div>
-  )
+    <Container maxW="container.xl" px={4} pt={5}>
+      <Navbar />
+      <Heading as="h1" size="xl" mb={5} textAlign="center">
+        Liste des Utilisateurs
+      </Heading>
+      <SimpleGrid
+        columns={{ sm: 2, md: 4 }}
+        spacing="8"
+        my={4}
+        justifyContent="center"
+      >
+        {users.map((user) => (
+          <Box
+            shadow="md"
+            borderWidth="1px"
+            p={5}
+            key={uuidv4()}
+            flex="nowrap"
+            justifyContent="space-between"
+          >
+            <Heading
+              as="h5"
+              fontSize={{ sm: "md", md: "lg", xl: "xl" }}
+              ml="auto"
+            >
+              {user.username}
+            </Heading>
+
+            <Text as="p" fontSize={{ sm: "md", md: "lg", xl: "xl" }}>
+              {user.phone}
+            </Text>
+
+            <NextLink href={`/users/${user.id.toString()}`} passHref>
+              <Link>Contact</Link>
+            </NextLink>
+          </Box>
+        ))}
+      </SimpleGrid>
+    </Container>
+  );
+}
+
+export async function getStaticProps() {
+  const data = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await data.json();
+
+  return {
+    props: {
+      users,
+    },
+  };
 }
